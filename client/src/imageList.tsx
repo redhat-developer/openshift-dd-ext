87ebcda0cd51f696700013a7319e7e3f78cb8f6f
Fred Bricon <fbricon@gmail.com>
2022-04-05 10:34:10 +0200

    Make imageList more type-safe
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

--- client/src/imageList.tsx
+++ client/src/imageList.tsx
@@ -10,75 +10,87 @@ const { useState, useEffect } = React;
 const ddClient = createDockerDesktopClient();
 
 
+
 export function DockerImageList() {
-    const [selectedImage, setSelectedImage] = useState('');
-    const [images, setImages] = useState([]);
-    const [originalImages, setOriginalImages] = useState([]);
-    const [loading, setLoading] = useState(true);
+  interface DockerImage {
+    RepoTags: string[]
+  }
+
+  interface SelectOption {
+    label: string,
+    value: string
+  }
+
+  interface RowItem {
+    index: number
+    style: any
+  }
+
+  const [selectedImage, setSelectedImage] = useState('');
+  const [images, setImages] = useState<SelectOption[]>([]);
+  const [originalImages, setOriginalImages] = useState<SelectOption[]>([]);
+  const [loading, setLoading] = useState(true);
 
 
-    async function loadImages(): Promise<void> {
-        const data: any = await ddClient.docker.listImages();
-        const items = data.map((i: { RepoTags: any; }) => i.RepoTags).flat().filter((i: string | null) => i != null && '<none>:<none>' !== i).flat();
-        const options = items.map((d: any) => ({
-          value: d,
-          label: d
-        }))
-        setLoading(false);
-        setImages(options);
-        setOriginalImages(options);
-        return;
-    }
+  async function loadImages(): Promise<void> {
+    const data = (await ddClient.docker.listImages()) as DockerImage[];
+    const items = data.map(i => i.RepoTags).flat().filter(i => i != null && '<none>:<none>' !== i).flat().sort();
+    const options = items.map(d => ({
+      value: d,
+      label: d
+    } as SelectOption));
+    setLoading(false);
+    setImages(options);
+    setOriginalImages(options);
+    return;
+  }
 
-    useEffect(()=> {
-        if (loading) {
-            loadImages();
-        }
-    }, [loading]);
-
-    function handleDeploy(image: {label: string, value: string}) {
-        ddClient.desktopUI.toast.success(`Deployed ${image.label}!`);
-    }
-
-    function handleSearch(event: any) {
-        const trimmedValue = event.target.value.trim();
-        if(trimmedValue.length > 0) {
-            setImages(originalImages.filter((value: {label: string, value: string})=>value.label.includes(trimmedValue)));
-        } else {
-            setImages(originalImages)
-        }
-    }
-
-    const Row = (item: { index:number, style: any }): JSX.Element  => {
-        return ( 
-            <div style={item.style}>
-                {(images[item.index] as any).label}<Button onClick={()=>handleDeploy(images[item.index])}>Deploy</Button>
-            </div>
-      )};    
-
+  useEffect(() => {
     if (loading) {
-        return ( <p>Loading list of images.</p>)
+      loadImages();
     }
+  }, [loading]);
+
+  function handleDeploy(image: SelectOption) {
+    ddClient.desktopUI.toast.success(`Deployed ${image.label}!`);
+  }
+
+  function handleSearch(event: any) {
+    const trimmedValue = event.target.value.trim();
+    if (trimmedValue.length > 0) {
+      setImages(originalImages.filter((value) => value.label.includes(trimmedValue)));
+    } else {
+      setImages(originalImages)
+    }
+  }
+
+  const Row = (item: RowItem): JSX.Element => {
     return (
-        <>
-            <Box margin="15px 0px 15px 0px">Search: <input type="text" onChange={handleSearch}/></Box>
-            <div style={{ flex: '1 1 auto' }}>
-            <AutoSizer onResize={(size) => console.log(size)}>
-                {({width, height}) => (
-                    <ImageList
-                        itemCount={images.length}
-                        width={width}
-                        itemSize={()=>35}
-                        height={height}>
-                        {Row}
-                    </ImageList>
-                )}
-            </AutoSizer>
-            </div> 
-        </>
+      <div style={item.style}>
+        {images[item.index].label}<Button onClick={() => handleDeploy(images[item.index])}>Deploy</Button>
+      </div>
     )
+  };
+
+  if (loading) {
+    return (<p>Loading list of images.</p>)
+  }
+  return (
+    <>
+      <Box margin="15px 0px 15px 0px">Search: <input type="text" onChange={handleSearch} /></Box>
+      <div style={{ flex: '1 1 auto' }}>
+        <AutoSizer onResize={(size) => console.log(size)}>
+          {({ width, height }) => (
+            <ImageList
+              itemCount={images.length}
+              width={width}
+              itemSize={() => 35}
+              height={height}>
+              {Row}
+            </ImageList>
+          )}
+        </AutoSizer>
+      </div>
+    </>
+  )
 }
-
-
-
-
