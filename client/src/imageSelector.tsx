eb380a89e3158a0d89dc871ec0339708e215513b
Denis Golovin <dgolovin@redhat.com>
2022-04-06 00:12:31 -0700

    Use styles for combo from current react theme
    Signed-off-by: Denis Golovin dgolovin@redhat.com

--- client/src/imageSelector.tsx
+++ client/src/imageSelector.tsx
@@ -1,16 +1,17 @@
 import { Button } from "@mui/material";
-import { Box } from "@mui/system";
+import { Box, styled } from "@mui/system";
 import { useEffect, useState } from "react";
-import Select, { SingleValue } from "react-select";
+import Select, { SingleValue, StylesConfig } from "react-select";
 import { getLocalImages } from "./utils/DockerUtils";
 import { createDockerDesktopClient } from '@docker/extension-api-client';
+import { useTheme } from '@mui/material';
 
 interface ImageOption {
   readonly value: string;
   readonly label: string;
 }
 
-export function ImageSelector() {
+export function ImageSelector(props: any) {
   const [loading, setLoading] = useState(true);
   const [defaultImage, setDefaultImage] = useState('');
   const [images, setImages] = useState<ImageOption[]>([]);
@@ -49,7 +50,7 @@ export function ImageSelector() {
       onImageSelection(image?.value);
     }
   }
-
+  const dockerTheme = useTheme();
   return (
     <div className='flexbox-container'>
       <Box margin="15px 0px 15px 0px" width="70%">
@@ -61,6 +62,29 @@ export function ImageSelector() {
           options={images}
           onChange={handleSelection}
           value={images.filter(image => image.label === selectedImage)}
+          theme={(theme) => ({
+            ...theme,
+            colors: {
+              ...theme.colors,
+              // primary: 'hotpink' // selected input border color
+              primary25: dockerTheme.palette.primary.light, // option highlight color
+              // primary50: 'hotpink' // unknown
+              // primary75: 'hotpink' //unknown 
+              neutral0: dockerTheme.palette.background.default, // background
+              // neutral5: 'hotpink',
+              // neutral10: 'green',
+              // neutral20: 'yellow', // inactive input field border
+              // neutral30: 'blue',
+              // neutral40: 'hotpink', // mouse hoover over x to clean filter and combo button \/ icon
+              neutral50: dockerTheme.palette.text.secondary, // input field placeholder text
+              // neutral60: 'yellow', // selected combo x and \/ icon colors
+              // neutral70: 'hotpink' // unknown
+              neutral80: dockerTheme.palette.text.secondary, // text color for selectd value in input field
+              // neutral90: 'yellow', // unknown
+              // danger: 'hotpink' // unknown
+
+            },
+          })}
         />
       </Box>
       <Button variant="contained" onClick={deploy} disabled={!selectedImage}> Deploy {selectedImage} </Button>
