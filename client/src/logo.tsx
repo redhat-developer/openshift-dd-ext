a4c0ee83d53202165cc8c770c585f80a0f0c8b39
Denis Golovin <dgolovin@redhat.com>
2022-04-19 10:56:41 -0700

    Move logo part in separate file logo.tsx
    Signed-off-by: Denis Golovin dgolovin@redhat.com

---
+++ client/src/logo.tsx
@@ -0,0 +1,26 @@
+import { Box, Typography } from '@mui/material';
+import logo from './images/logo.png';
+
+export default function Logo() {
+
+  // TODO use styles.
+  const iconContainer = {
+    height: 60,
+    marginBottom: '3em',
+    marginTop: '2em',
+    width: '100%',
+  };
+
+  const logoStyle = {
+    maxHeight: '100%',
+    maxWidth: '100%'
+  }
+
+  return (
+    <Box style={{ textAlign: 'center', marginBottom: '20px' }}>
+      <Box style={iconContainer}>
+        <img src={logo} style={logoStyle} />
+      </Box>
+    </Box >
+  );
+}
