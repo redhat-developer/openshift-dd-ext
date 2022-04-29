997ac2f61a8d9e9ab41e5968afa5fb5d2cff4b9c
Denis Golovin <dgolovin@redhat.com>
2022-04-28 20:48:49 -0700

    Add recoil to dependencies and use react alert component
    Signed-off-by: Denis Golovin dgolovin@redhat.com

--- client/src/imageSelector.tsx
+++ client/src/imageSelector.tsx
@@ -1,4 +1,4 @@
-import { Button, Paper, Tooltip } from "@mui/material";
+import { Alert, Button, Link, Paper, Tooltip } from "@mui/material";
 import { Box } from "@mui/system";
 import { useEffect, useState } from "react";
 import Select, { SingleValue } from "react-select";
@@ -27,7 +27,7 @@ export function ImageSelector(props?: ImageSelectorProps) {
   const [images, setImages] = useState<Map<string, IDockerImage>>(new Map());
   const [imageOptions, setImageOptions] = useState<ImageOption[]>([]);
   const [selectedImage, setSelectedImage] = useState<ISelectedImage | null>(null);
-  const [currentContext, ] = useRecoilState(currentContextState);
+  const [currentContext,] = useRecoilState(currentContextState);
 
   async function loadImages(): Promise<void> {
     const refreshing = !initialLoading;
@@ -83,63 +83,65 @@ export function ImageSelector(props?: ImageSelectorProps) {
       return <span>Select a context to deploy</span>;
     } else if (!selectedImage) {
       return <span>Select an image to deploy</span>;
-    } 
-    return <span>Deploying {selectedImage?.name} to project '{currentContext.project}' on <a href="#" onClick={()=> openInBrowser(currentContext.clusterUrl!!)}>{currentContext.clusterUrl}</a></span>;
+    }
+    return <>Deploying {selectedImage?.name} to project '{currentContext.project}' on <Link color="inherit" href="#" onClick={() => openInBrowser(currentContext.clusterUrl!!)}>{currentContext.clusterUrl}</Link></>;
   }
 
   const dockerTheme = useTheme();
   return (
     <>
-    <Box margin="15px 0px 15px 0px" display="flex" flexDirection="row">
-      <div style={{ flex: '1 1 auto' }}>
-        <Select
-          noOptionsMessage={() => "No images found"}
-          placeholder="Select an image to deploy"
-          isClearable
-          isSearchable
-          options={imageOptions}
-          onChange={handleSelection}
-          value={imageOptions.filter(image => image.value === selectedImage?.name)}
-          theme={(theme) => ({
-            ...theme,
-            colors: {
-              ...theme.colors,
-              // primary: 'hotpink' // selected input border color
-              primary25: dockerTheme.palette.primary.light, // option highlight color
-              // primary50: 'hotpink' // unknown
-              // primary75: 'hotpink' //unknown 
-              neutral0: dockerTheme.palette.background.default, // background
-              // neutral5: 'hotpink',
-              // neutral10: 'green',
-              // neutral20: 'yellow', // inactive input field border
-              // neutral30: 'blue',
-              // neutral40: 'hotpink', // mouse hoover over x to clean filter and combo button \/ icon
-              neutral50: dockerTheme.palette.text.secondary, // input field placeholder text
-              // neutral60: 'yellow', // selected combo x and \/ icon colors
-              // neutral70: 'hotpink' // unknown
-              neutral80: dockerTheme.palette.text.secondary, // text color for selectd value in input field
-              // neutral90: 'yellow', // unknown
-              // danger: 'hotpink' // unknown
-            },
-          })}
-        />
-      </div>
-      <Tooltip title="Reload the list of local Docker images" placement='bottom-end'>
-        <span>
-          <Button variant="outlined" onClick={handleRefresh} style={{ marginLeft: '20px' }} disabled={loading}>
-            <RefreshRounded /> Reload
-          </Button>
-        </span>
-      </Tooltip>
-      <Tooltip title="Deploy the selected image to OpenShift" placement='bottom-end'>
-        <span>
-          <Button style={{ marginLeft: '20px' }} variant="contained" onClick={deploy} disabled={!selectedImage}> Deploy </Button>
-        </span>
-      </Tooltip>
-    </Box >
-    <Paper elevation={4} style={{backgroundColor:'white', padding:'10px', textAlign: 'center', marginBottom:'20px'}}>
-    {deploymentText()}
-    </Paper>
+      <Box margin="15px 0px 15px 0px" display="flex" flexDirection="row">
+        <div style={{ flex: '1 1 auto' }}>
+          <Select
+            noOptionsMessage={() => "No images found"}
+            placeholder="Select an image to deploy"
+            isClearable
+            isSearchable
+            options={imageOptions}
+            onChange={handleSelection}
+            value={imageOptions.filter(image => image.value === selectedImage?.name)}
+            theme={(theme) => ({
+              ...theme,
+              colors: {
+                ...theme.colors,
+                // primary: 'hotpink' // selected input border color
+                primary25: dockerTheme.palette.primary.light, // option highlight color
+                // primary50: 'hotpink' // unknown
+                // primary75: 'hotpink' //unknown 
+                neutral0: dockerTheme.palette.background.default, // background
+                // neutral5: 'hotpink',
+                // neutral10: 'green',
+                // neutral20: 'yellow', // inactive input field border
+                // neutral30: 'blue',
+                // neutral40: 'hotpink', // mouse hoover over x to clean filter and combo button \/ icon
+                neutral50: dockerTheme.palette.text.secondary, // input field placeholder text
+                // neutral60: 'yellow', // selected combo x and \/ icon colors
+                // neutral70: 'hotpink' // unknown
+                neutral80: dockerTheme.palette.text.secondary, // text color for selectd value in input field
+                // neutral90: 'yellow', // unknown
+                // danger: 'hotpink' // unknown
+              },
+            })}
+          />
+        </div>
+        <Tooltip title="Reload the list of local Docker images" placement='bottom-end'>
+          <span>
+            <Button variant="outlined" onClick={handleRefresh} style={{ marginLeft: '20px' }} disabled={loading}>
+              <RefreshRounded /> Reload
+            </Button>
+          </span>
+        </Tooltip>
+        <Tooltip title="Deploy the selected image to OpenShift" placement='bottom-end'>
+          <span>
+            <Button style={{ marginLeft: '20px' }} variant="contained" onClick={deploy} disabled={!selectedImage}> Deploy </Button>
+          </span>
+        </Tooltip>
+      </Box>
+      <Box marginBottom="15px">
+        <Alert variant="filled" severity="info">
+          {deploymentText()}
+        </Alert>
+      </Box>
     </>
   );
 }
