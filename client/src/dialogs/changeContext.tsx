9383c25ed46d9239a3c610c8f325689617a00268
Fred Bricon <fbricon@gmail.com>
2022-05-19 18:46:50 +0200

    Display link to Login from context
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

--- client/src/dialogs/changeContext.tsx
+++ client/src/dialogs/changeContext.tsx
@@ -1,17 +1,15 @@
-import * as React from 'react';
-import { useEffect, useState } from 'react';
+import { createDockerDesktopClient } from '@docker/extension-api-client';
+import { Box, CircularProgress, Divider, List, ListItem, ListItemButton, ListItemText, Typography } from '@mui/material';
 import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
-import { isPropertyAssignment } from 'typescript';
-import { Backdrop, Box, CircularProgress, Divider, List, ListItem, ListItemAvatar, ListItemButton, ListItemText, Typography } from '@mui/material';
-import { loadContextUiData, readKubeConfig, setCurrentContext } from '../utils/OcUtils';
-import { createDockerDesktopClient } from '@docker/extension-api-client';
+import * as React from 'react';
 import { UnknownKubeContext } from '../models/KubeContext';
+import { loadContextUiData, readKubeConfig, setCurrentContext } from '../utils/OcUtils';
+import { LoginDialog } from './login';
 
 interface LoginDialogProps {
   install: (showDialog: () => void) => void;
@@ -54,75 +52,94 @@ export function ChangeContext(props: LoginDialogProps) {
     });
   }
 
+  const openLoginPage = () => {
+    setOpen(false);
+    showLoginDialog();
+  }
+
   props.install(handleOpen);
+
+
+  let showLoginDialog: () => void;
+
+  const installLoginDialog = (showDialogHandler: () => void) => {
+    showLoginDialog = showDialogHandler;
+  }
+
+  const ctxDescription = contexts.length > 0 ? 'Select Context from the list below' : 'No context available';
 
   return (
     <div>
       <Dialog open={open} onClose={handleChange} fullWidth={true}>
         <DialogTitle>Change Context</DialogTitle>
         <DialogContent>
-          <DialogContentText paddingBottom="16px">
-            <Typography
-              component="span"
-              variant="body1"
-              color="text.primary">
-              Select Context from the list below
-            </Typography>
-          </DialogContentText>
           {(loading) && (
             <Box width="100%" component="div" display="flex" alignContent="center" justifyContent="center" padding="20px">
               <CircularProgress color="inherit" />
             </Box>
           )}
           {(!loading) && (
-            <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
-              {contexts.map((item: { name: string }, index: number) => {
-                const contextUiData = loadContextUiData(kubeConfig, item.name);
-                return (contextUiData !== UnknownKubeContext) && (
-                  <React.Fragment key={index}>
-                    <ListItem>
-                      <ListItemButton alignItems="flex-start" selected={item.name === selectedContext} onClick={() => setSelectedContext(item.name)}>
-                        <ListItemText
-                          primary={
-                            <Typography
-                              sx={{
-                                whiteSpace: "nowrap",
-                                overflow: "hidden",
-                                textOverflow: "ellipsis",
-                                padding: "2px"
-                              }}
-                              variant="h6">
-                              {item.name}
-                            </Typography>
-                          }
-                          secondary={
-                            <Typography
-                              sx={{ display: 'inline' }}
-                              component="div"
-                              variant="body2"
-                              color="text.primary"
-                            >
-                              <div><b>Cluster:</b> {contextUiData.clusterUrl}</div>
-                              <div><b>Username:</b> {contextUiData.user}</div>
-                              <div><b>Project:</b> {contextUiData.project}</div>
-                            </Typography>
-                          }
-                        />
-                      </ListItemButton>
-                    </ListItem>
-                    {(index + 1 < contexts.length) && (<Divider variant="fullWidth" />)}
-                  </React.Fragment>
-                )
-              })
-              }
-            </List>
+            <>
+              <DialogContentText paddingBottom="16px">
+                <Typography
+                  component="span"
+                  variant="body1"
+                  color="text.primary">
+                  {ctxDescription}
+                </Typography>
+              </DialogContentText>
+              <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
+                {contexts.map((item: { name: string }, index: number) => {
+                  const contextUiData = loadContextUiData(kubeConfig, item.name);
+                  return (contextUiData !== UnknownKubeContext) && (
+                    <React.Fragment key={index}>
+                      <ListItem>
+                        <ListItemButton alignItems="flex-start" selected={item.name === selectedContext} onClick={() => setSelectedContext(item.name)}>
+                          <ListItemText
+                            primary={
+                              <Typography
+                                sx={{
+                                  whiteSpace: "nowrap",
+                                  overflow: "hidden",
+                                  textOverflow: "ellipsis",
+                                  padding: "2px"
+                                }}
+                                variant="h6">
+                                {item.name}
+                              </Typography>
+                            }
+                            secondary={
+                              <Typography
+                                sx={{ display: 'inline' }}
+                                component="div"
+                                variant="body2"
+                                color="text.primary"
+                              >
+                                <div><b>Cluster:</b> {contextUiData.clusterUrl}</div>
+                                <div><b>Username:</b> {contextUiData.user}</div>
+                                <div><b>Project:</b> {contextUiData.project}</div>
+                              </Typography>
+                            }
+                          />
+                        </ListItemButton>
+                      </ListItem>
+                      {(index + 1 < contexts.length) && (<Divider variant="fullWidth" />)}
+                    </React.Fragment>
+                  )
+                })
+                }
+              </List>
+            </>
           )}
         </DialogContent>
         <DialogActions sx={{ padding: "0 24px 20px 24px" }}>
+          <Button variant="contained" disabled={loading} onClick={openLoginPage}>Login to an OpenShift Cluster</Button>
+          <div style={{ flex: '1 0 0' }} />
           <Button variant="outlined" onClick={handleCancel}>Cancel</Button>
           <Button variant="contained" disabled={loading || !kubeConfig || kubeConfig["current-context"] === selectedContext} onClick={handleChange}>Change</Button>
         </DialogActions>
       </Dialog>
+      <LoginDialog install={installLoginDialog} onLogin={() => { props.onContextChange() }} />
     </div>
   );
 }
