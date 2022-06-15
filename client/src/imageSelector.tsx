8a062a7793ad44e8e6a0fb8cb149b5ffd7506d01
Denis Golovin <dgolovin@redhat.com>
2022-06-14 21:27:25 -0700

    Use MUI Autocomplete in place of react-select
    This PR fixes #60.
    
    Signed-off-by: Denis Golovin dgolovin@redhat.com

--- client/src/imageSelector.tsx
+++ client/src/imageSelector.tsx
@@ -1,9 +1,9 @@
-import { Alert, Button, Link, Paper, Tooltip } from "@mui/material";
+import { Alert, Autocomplete, Button, Link, MenuItem, Paper, TextField, Tooltip } from "@mui/material";
 import { Box } from "@mui/system";
 import { useEffect, useState } from "react";
-import Select, { SingleValue } from "react-select";
 import { getLocalImages } from "./utils/DockerUtils";
 import { useTheme } from '@mui/material';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
 import { IDockerImage, ISelectedImage } from "./models/IDockerImage";
 import { RefreshRounded } from "@mui/icons-material";
 import { openInBrowser, toast } from "./utils/UIUtils";
@@ -66,14 +66,16 @@ export default function ImageSelector(props?: ImageSelectorProps) {
     }
   }
 
-  function handleSelection(imageOption: SingleValue<ImageOption>) {
-    console.log(`Selected image: ${imageOption?.value}`);
-    const name = imageOption?.value;
-    if (name) {
+  function handleSelection(event: any, imageOption: ImageOption | null) {
+    console.log(`Selected image: ${imageOption}`);
+    if (imageOption) {
+      const name = imageOption?.label;
       const image = images.get(name);
       if (image) {
         setSelectedImage({ name, image });
       }
+    } else {
+      setSelectedImage(null);
     }
   }
 
@@ -90,53 +92,42 @@ export default function ImageSelector(props?: ImageSelectorProps) {
     return <>Deploying {selectedImage?.name} to project '{currentContext.project}' on <Link color="inherit" href="#" onClick={() => openInBrowser(currentContext.clusterUrl!!)}>{currentContext.clusterUrl}</Link></>;
   }
 
-  const dockerTheme = useTheme();
   return (
     <>
       <Box margin="15px 0px 15px 0px" display="flex" flexDirection="row">
         <div style={{ flex: '1 1 auto' }}>
-          <Select
-            noOptionsMessage={() => "No images found"}
+          <Autocomplete
+            size="small"
+            // noOptionsMessage={() => "No images found"}
             placeholder="Select an image to deploy"
-            isClearable
-            isSearchable
+            // isClearable
+            // isSearchable
             options={imageOptions}
             onChange={handleSelection}
-            value={imageOptions.filter(image => image.value === selectedImage?.name)}
-            theme={(theme) => ({
-              ...theme,
-              colors: {
-                ...theme.colors,
-                // primary: 'hotpink' // selected input border color
-                primary25: dockerTheme.palette.primary.light, // option highlight color
-                // primary50: 'hotpink' // unknown
-                // primary75: 'hotpink' //unknown 
-                neutral0: dockerTheme.palette.background.default, // background
-                // neutral5: 'hotpink',
-                // neutral10: 'green',
-                // neutral20: 'yellow', // inactive input field border
-                // neutral30: 'blue',
-                // neutral40: 'hotpink', // mouse hoover over x to clean filter and combo button \/ icon
-                neutral50: dockerTheme.palette.text.secondary, // input field placeholder text
-                // neutral60: 'yellow', // selected combo x and \/ icon colors
-                // neutral70: 'hotpink' // unknown
-                neutral80: dockerTheme.palette.text.secondary, // text color for selectd value in input field
-                // neutral90: 'yellow', // unknown
-                // danger: 'hotpink' // unknown
-              },
-            })}
+            // value={imageOptions.filter(image => image.value === selectedImage?.name)}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Select an image to deploy"
+                inputProps={{
+                  ...params.inputProps,
+                  autoComplete: 'new-password', // disable autocomplete and autofill
+                }}
+              />
+            )}
+
           />
         </div>
         <Tooltip title="Reload the list of local Docker images" placement='bottom-end'>
           <span>
-            <Button variant="outlined" onClick={handleRefresh} style={{ marginLeft: '20px' }} disabled={loading}>
+            <Button size="large" variant="outlined" onClick={handleRefresh} style={{ marginLeft: '20px' }} disabled={loading}>
               <RefreshRounded /> Reload
             </Button>
           </span>
         </Tooltip>
         <Tooltip title="Deploy the selected image to OpenShift" placement='bottom-end'>
           <span>
-            <Button style={{ marginLeft: '20px' }} variant="contained" onClick={deploy} disabled={!selectedImage}> Deploy </Button>
+            <Button size="large" style={{ marginLeft: '20px' }} variant="contained" onClick={deploy} disabled={!selectedImage}> Deploy </Button>
           </span>
         </Tooltip>
       </Box>
